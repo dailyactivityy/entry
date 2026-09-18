f988c7174334f70e33ed0843a82c21b9e610dd84
dailyactivityy <r79128842@gmail.com>
2026-09-18 12:13:55 +0530

    Delete js/config.js

--- js/config.js
+++
@@ -1,3 +0,0 @@
-// Paste the Web App /exec URL you get after deploying apps-script/Code.gs
-// Apps Script Deploy > New deployment > Web app > Execute as: Me, Access: Anyone
-const API_URL = "https://script.google.com/macros/s/AKfycbzmm1Ot2nOgGpAyQ5GtKbF7jVjlJezZsclFR1oNCbpCi_ycbFg0Bc1uFFlgIhBp0y9F/exec";
